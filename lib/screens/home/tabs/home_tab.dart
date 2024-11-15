640243aaa7e0e8f7318b4ea4098d4a0946a43439
laspi <pro43071919@gmail.com>
2024-11-15 18:08:54 +0100

    add detail page

--- lib/screens/home/tabs/home_tab.dart
+++ lib/screens/home/tabs/home_tab.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:register/screens/add_page.dart';
-
+import 'package:register/screens/detail_page.dart';
 import '../../../config/theme.dart';
-
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -113,106 +112,130 @@ class HomeTab extends StatelessWidget {
                   padding: const EdgeInsets.all(16),
                   itemCount: 10,
                   itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.2),
-                            spreadRadius: 2,
-                            blurRadius: 8,
-                            offset: const Offset(0, 3),
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailPage(
+                              title: 'Villa ${quartiers[index]}',
+                              price: '${prix[index]} FCFA',
+                              location: quartiers[index],
+                              description: 'Detailed description about the property...',
+                              features: [
+                                '${(index + 2) * 25} m²',
+                                '${index + 2} chambres',
+                                '${index + 1} sdb',
+                              ],
+                              images: [
+                                'https://picsum.photos/seed/${index + 1}/400/250',
+                                'https://picsum.photos/seed/${index + 2}/400/250',
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Container(
-                          color: Colors.white,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Stack(
-                                children: [
-                                  Image.network(
-                                    'https://picsum.photos/seed/${index + 1}/400/250',
-                                    height: 200,
-                                    width: double.infinity,
-                                    fit: BoxFit.cover,
-                                  ),
-                                  Positioned(
-                                    top: 10,
-                                    right: 10,
-                                    child: Container(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 12, vertical: 6),
-                                      decoration: BoxDecoration(
-                                        color: AppTheme.primaryColor,
-                                        borderRadius: BorderRadius.circular(20),
-                                      ),
-                                      child: Text(
-                                        '${prix[index]} FCFA',
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 8,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Container(
+                            color: Colors.white,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Stack(
                                   children: [
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'Villa ${quartiers[index]}',
+                                    Image.network(
+                                      'https://picsum.photos/seed/${index + 1}/400/250',
+                                      height: 200,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Positioned(
+                                      top: 10,
+                                      right: 10,
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 12, vertical: 6),
+                                        decoration: BoxDecoration(
+                                          color: AppTheme.primaryColor,
+                                          borderRadius: BorderRadius.circular(20),
+                                        ),
+                                        child: Text(
+                                          '${prix[index]} FCFA',
                                           style: const TextStyle(
-                                            fontSize: 20,
+                                            color: Colors.white,
                                             fontWeight: FontWeight.bold,
                                           ),
                                         ),
-                                        IconButton(
-                                          icon: const Icon(Icons.favorite_border),
-                                          onPressed: () {},
-                                          color: AppTheme.primaryColor,
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 8),
-                                    Row(
-                                      children: [
-                                        const Icon(Icons.location_on,
-                                            color: Colors.grey, size: 16),
-                                        const SizedBox(width: 4),
-                                        Text(
-                                          quartiers[index],
-                                          style: const TextStyle(color: Colors.grey),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 12),
-                                    Row(
-                                      children: [
-                                        _buildFeature(
-                                            Icons.square_foot, '${(index + 2) * 25} m²'),
-                                        const SizedBox(width: 24),
-                                        _buildFeature(
-                                            Icons.bed, '${index + 2} chambres'),
-                                        const SizedBox(width: 24),
-                                        _buildFeature(
-                                            Icons.bathroom, '${index + 1} sdb'),
-                                      ],
+                                      ),
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
+                                Padding(
+                                  padding: const EdgeInsets.all(16),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            'Villa ${quartiers[index]}',
+                                            style: const TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          IconButton(
+                                            icon: const Icon(Icons.favorite_border),
+                                            onPressed: () {},
+                                            color: AppTheme.primaryColor,
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 8),
+                                      Row(
+                                        children: [
+                                          const Icon(Icons.location_on,
+                                              color: Colors.grey, size: 16),
+                                          const SizedBox(width: 4),
+                                          Text(
+                                            quartiers[index],
+                                            style: const TextStyle(color: Colors.grey),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 12),
+                                      Row(
+                                        children: [
+                                          _buildFeature(Icons.square_foot,
+                                              '${(index + 2) * 25} m²'),
+                                          const SizedBox(width: 24),
+                                          _buildFeature(
+                                              Icons.bed, '${index + 2} chambres'),
+                                          const SizedBox(width: 24),
+                                          _buildFeature(
+                                              Icons.bathroom, '${index + 1} sdb'),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
